c79deacfa6769fab728c3bb1c380a2d422f00338
yeonseo <yulga_nara@naver.com>
2020-07-06 15:46:44 +0900

    기사 페이지를 get방식으로 요청 시, 기사제목으로 데이터 찾은 후 render
    기사 페이지를 get방식으로 요청 시,
    기사제목으로 데이터를 찾은 후 render

--- blog/src/main/kotlin/com/springBackendTest/blog/BlogApplication.kt
+++ blog/src/main/kotlin/com/springBackendTest/blog/BlogApplication.kt
@@ -1,5 +1,6 @@
 package com.springBackendTest.blog
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,6 +9,6 @@ class BlogApplication
 
 fun main(args: Array<String>) {
 	runApplication<BlogApplication>(*args){
-		// setBannerMode(Banner.Mode.OFF)
+		setBannerMode(Banner.Mode.OFF)
 	}
 }
